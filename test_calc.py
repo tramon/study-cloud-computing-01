8def0136fc803ce95255bce0331b507f5c99baaf
tramon <andriitramon@gmail.com>
2024-10-23 10:34:35 +0300

    recover failing test - fix 01

--- test_calc.py
+++ test_calc.py
@@ -5,9 +5,9 @@ def test_sum():
     assert sum_calculation(2, 3) == 5
 
 
-# @pytest.mark.skipif(reason="known defect")
+#@pytest.mark.skipif(reason="known defect")
 def test_tricky_divide():
     expected_result = 2
-    actual_result = tricky_divide(12, 6) == expected_result
+    actual_result = tricky_divide(12, 6)
     assert actual_result == expected_result, \
         f"Divide results failed. Expected {expected_result} but got {actual_result}"
